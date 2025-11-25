4d23a9191e458792d465ef85f9f934ae16da4743
Eduardo Dourado <162699065+edudouraado@users.noreply.github.com>
2025-11-25 16:28:47 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,12 @@ from flask_cors import CORS
 from controllers.carro_controller import carro_bp
 
 app = Flask(__name__)
-CORS(app) # Permite que o Front converse com o Back
+CORS(app) # Permite o Front conversar com o Back
+
+# Rota nova para a página inicial não dar 404
+@app.route('/')
+def home():
+    return "API da Garagem Motors está rodando! 🚗💨 Use a rota /carros"
 
 # Registra as rotas
 app.register_blueprint(carro_bp)
